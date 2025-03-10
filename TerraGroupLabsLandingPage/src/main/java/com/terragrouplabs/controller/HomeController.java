a9da7bd50089e1c11055f8bd3297b09caf717003
NamakonoTaiji <kazumizu0201@gmail.com>
2025-03-10 23:21:32 +0900

    サービス・会社概要セクションを実装

--- TerraGroupLabsLandingPage/src/main/java/com/terragrouplabs/controller/HomeController.java
+++ TerraGroupLabsLandingPage/src/main/java/com/terragrouplabs/controller/HomeController.java
@@ -1,18 +1,39 @@
 package com.terragrouplabs.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.terragrouplabs.entity.ContactMessage;
+import com.terragrouplabs.service.ServiceService;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private ServiceService serviceService;
 
     @GetMapping("/")
     public String showHomePage(Model model) {
         // フォームオブジェクトを初期化
         model.addAttribute("contactMessage", new ContactMessage());
+        
+        // サービス一覧をモデルに追加
+        model.addAttribute("services", serviceService.getAllServices());
+        
         return "index";
+    }
+    
+    @GetMapping("/about")
+    public String showAboutPage() {
+        return "about";
+    }
+    
+    @GetMapping("/service")
+    public String showServicePage(Model model) {
+        // サービス一覧をモデルに追加
+        model.addAttribute("services", serviceService.getAllServices());
+        return "service";
     }
 }
