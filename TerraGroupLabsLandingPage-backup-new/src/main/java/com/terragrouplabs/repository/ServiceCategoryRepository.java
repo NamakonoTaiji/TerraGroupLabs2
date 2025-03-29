5290f9c07bf5ae82d2fdce84a17760a785c3463a
NamakonoTaiji <kazumizu0201@gmail.com>
2025-03-29 21:40:34 +0900

    余分なファイルもコミットしていたので修正

--- TerraGroupLabsLandingPage-backup-new/src/main/java/com/terragrouplabs/repository/ServiceCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.terragrouplabs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.terragrouplabs.entity.ServiceCategory;
-
-public interface ServiceCategoryRepository extends JpaRepository<ServiceCategory, Long> {
-    // 必要に応じてカスタムメソッドを追加
-}
